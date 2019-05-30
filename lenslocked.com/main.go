ad1e25b6950664014dc67e281cc24742b9299198
msarah <murphy.sarahlf@gmail.com>
2019-05-30 10:13:13 -0400

    added more routes

--- lenslocked.com/main.go
+++ lenslocked.com/main.go
@@ -20,26 +20,40 @@ the user may have sent with the request e.g. email for sign in.
 */
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
-	/*
-	  Fprint takes
-	  (1) and io.Writer to write to and
-	  (2)Any number of interface{}s to print out. Typically strings, but could be any data type.
-
-	  An io.Writer is an interface that requires a struct to have implemented the Write([]byte) method
-	  so fmt.Fprint helps handle converting all of the provided interfaces to a byte array,
-	  and then calls the Write method on the io.Writer.
-
-	  Since we are writing a string, and strings can be treated as byte arrays,
-	  you could replace the line
-
-	  fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>") with
-	  w.Write([]byte("<h1>Welcome to my awesome site!</h1>"))
-
-	  and you would end up getting the same end result.
-	*/
-
+	fmt.Println(r)
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email "+
+			"to <a href=\"mailto:support@lenslocked.com\">"+
+			"support@lenslocked.com</a>.")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>We could not find the page you "+
+			"were looking for :(</h1>"+
+			"<p>Please email us if you keep being sent to an "+
+			"invalid page.</p>")
+	}
 }
+
+/*
+  Fprint takes
+  (1) and io.Writer to write to and
+  (2)Any number of interface{}s to print out. Typically strings, but could be any data type.
+
+  An io.Writer is an interface that requires a struct to have implemented the Write([]byte) method
+  so fmt.Fprint helps handle converting all of the provided interfaces to a byte array,
+  and then calls the Write method on the io.Writer.
+
+  Since we are writing a string, and strings can be treated as byte arrays,
+  you could replace the line
+
+  fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>") with
+  w.Write([]byte("<h1>Welcome to my awesome site!</h1>"))
+
+  and you would end up getting the same end result.
+*/
 
 func main() {
 	http.HandleFunc("/", handlerFunc) // "/" handles all path requests
