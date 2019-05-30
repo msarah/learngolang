ed973a306207a6e6f00bd067a6adc7ab9d2a0a17
msarah <murphy.sarahlf@gmail.com>
2019-05-30 09:19:39 -0400

    changes

--- lenslocked.com/main.go
+++ lenslocked.com/main.go
@@ -1,15 +1,47 @@
-package main
+package main //every go file must have a package name specified
 
-import (
-  "fmt"
-  "net/http"
+import ( //these are packages imported from the golang standard library
+	"fmt"      //formatting strings
+	"net/http" //tools for web development
 )
 
+/*
+handlerFunc takes a response writer and pointer to a request.
+These two objects allow us to transmit data over the web.
+When a user makes a request this function takes that request
+and uses the response writer object to send back a reply.
+
+We can do this because the ResponseWriter has the Write() method
+which is what we use to write to the response body. We can also use
+this object to write headers when we need to.
+
+The pointer to request object allows us to access any information
+the user may have sent with the request e.g. email for sign in.
+*/
+
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
+	fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
+	/*
+	  Fprint takes
+	  (1) and io.Writer to write to and
+	  (2)Any number of interface{}s to print out. Typically strings, but could be any data type.
+
+	  An io.Writer is an interface that requires a struct to have implemented the Write([]byte) method
+	  so fmt.Fprint helps handle converting all of the provided interfaces to a byte array,
+	  and then calls the Write method on the io.Writer.
+
+	  Since we are writing a string, and strings can be treated as byte arrays,
+	  you could replace the line
+
+	  fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>") with
+	  w.Write([]byte("<h1>Welcome to my awesome site!</h1>"))
+
+	  and you would end up getting the same end result.
+	*/
+
 }
 
 func main() {
-  http.HandleFunc("/", handlerFunc)
-  http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/", handlerFunc) // "/" handles all path requests
+	http.ListenAndServe(":3000", nil) // starts up a local web server using default handlers
 }
