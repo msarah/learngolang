f90cf21ade2b24544cc6b3fd4f5941561b8b90e9
msarah <murphy.sarahlf@gmail.com>
2019-06-07 16:59:04 -0400

    a lot of stuff

--- exercises/gotemplates/main.go
+++ exercises/gotemplates/main.go
@@ -2,24 +2,73 @@ package main
 
 import (
 	"html/template"
+	"net/http"
 	"os"
+
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	t, err := template.ParseFiles("hello.gohtml")
-	/*ParseFiles will open up the template file and attempt
-	  to validate it*/
-	if err != nil {
-		panic(err)
-	}
+var testTemplate *template.Template
 
-	data := struct {
-		Name string
-	}{"John Smith"}
-
-	err = t.Execute(os.Stdout, data)
-	if err != nil {
-		panic(err)
-	}
-
+type User struct {
+	Name          string
+	Uname         string //has to be capitalized in order to be exported
+	Age           int
+	FavouriteFood map[string]string
 }
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	data := User{
+		"Sarah Murphy",
+		"msarah",
+		22,
+		map[string]string{
+			// "Breakfast": "Oatmeal",
+			"Lunch":  "Sandwich",
+			"Dinner": "Pasta",
+		},
+	}
+
+	err := testTemplate.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+
+	var err error
+	testTemplate, err = template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	err = testTemplate.Execute(os.Stdout, User{
+		"Sarah Murphy",
+		"msarah",
+		22,
+		map[string]string{
+			"Breakfast": "Oatmeal",
+			"Lunch":     "Sandwich",
+			"Dinner":    "Pasta",
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	http.ListenAndServe(":8080", r)
+}
+
+// data.Name = "Sarah"
+//
+// err = t.Execute(os.Stdout, data)
+// //arg1 where you want it to spit the data out
+// //arg2 data to be transmitted
+// if err != nil {
+// 	panic(err)
+// }
--- lenslocked.com/main.go
+++ lenslocked.com/main.go
@@ -1,44 +1,53 @@
 package main //every go file must have a package name specified
 
 import ( //these are packages imported from the golang standard library
-	"fmt"      //formatting strings
+	//formatting strings
 	"net/http" //tools for web development
+	"text/template"
 
 	"github.com/gorilla/mux"
 )
 
+/*
+We shouldn't be using a global variable here
+but for development purposes we will for now
+*/
+
+var homeTemplate *template.Template
+var contactTemplate *template.Template
+
+//this will store our template
+
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "To get in touch, please send an email "+
-		"to <a href=\"mailto:support@lenslocked.com\">"+
-		"support@lenslocked.com</a>.")
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
-
-func faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>FAQ</h1>"+"\n<p> Frequently asked questions "+
-		"can be found here</p>")
-}
-
-func notFound(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1> Page not found! You must be lost</h1>"+
-		"\n<p>Head back to home and start from there</p>")
-}
-
-var nf http.Handler = http.HandlerFunc(notFound)
 
 func main() {
+	//we want to execute the template first before our router
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err) // usually handle errors more gracefully
+	}
+
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
+	if err != nil {
+		panic(err) // usually handle errors more gracefully
+	}
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
-	r.NotFoundHandler = nf
 
 	http.ListenAndServe(":3000", r) // starts up a local web server using our new gorilla handler
 }
