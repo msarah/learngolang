394a2818eaf88e106be736bbe4e9bc142efae382
msarah <murphy.sarahlf@gmail.com>
2019-06-14 12:25:03 -0400

    created signup form and template

--- lenslocked.com/main.go
+++ lenslocked.com/main.go
@@ -12,6 +12,7 @@ var (
 	homeView    *views.View
 	contactView *views.View
 	faqView     *views.View
+	signupView  *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -29,15 +30,22 @@ func faq(w http.ResponseWriter, r *http.Request) {
 	must(faqView.Render(w, nil))
 }
 
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(signupView.Render(w, nil))
+}
+
 func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 	faqView = views.NewView("bootstrap", "views/faq.gohtml")
+	signupView = views.NewView("bootstrap", "views/signup.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
+	r.HandleFunc("/signup", signup)
 
 	http.ListenAndServe(":3000", r)
 }
